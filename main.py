6649ee5f9be7cc46bb402c055d75977f61ea6429
sagnikCodes <114083986+sagnikCodes@users.noreply.github.com>
2023-12-30 12:11:59 +0530

    Update main.py

--- main.py
+++ main.py
@@ -36,47 +36,51 @@ st.markdown(custom_css, unsafe_allow_html=True)
 
 
 uploaded_file = st.file_uploader("Upload an Image", type=["jpg", "jpeg", "png"])
-if uploaded_file is not None:
-    image_bytes = uploaded_file.read()
-    image_str = base64.b64encode(image_bytes).decode()
+try:
+    if uploaded_file is not None:
+        image_bytes = uploaded_file.read()
+        image_str = base64.b64encode(image_bytes).decode()
 
-    st.markdown(
-        f'<div style="display: flex; justify-content: center;"><img src="data:image/png;base64,{image_str}" alt="Uploaded Image" style="height:300px;"></div>',
-        unsafe_allow_html=True,
-    )
+        st.markdown(
+            f'<div style="display: flex; justify-content: center;"><img src="data:image/png;base64,{image_str}" alt="Uploaded Image" style="height:300px;"></div>',
+            unsafe_allow_html=True,
+        )
 
-    image = plt.imread(uploaded_file)
-    image = image.reshape(-1, 3)
+        image = plt.imread(uploaded_file)
+        image = image.reshape(-1, 3)
 
-    user_number = int(st.number_input("Enter how many dominant colors you want to see", min_value=0, max_value=100, value=5, step=1))
-    kmeans = KMeans(n_clusters=user_number)
-    kmeans.fit(image)
+        user_number = int(st.number_input("Enter how many dominant colors you want to see", min_value=0, max_value=100, value=5, step=1))
+        kmeans = KMeans(n_clusters=user_number)
+        kmeans.fit(image)
 
-    # Plotly Bar Chart with Hover Text
-    colors_hex = ['#%02x%02x%02x' % tuple(rgb) for rgb in kmeans.cluster_centers_.astype(int)]
+        # Plotly Bar Chart with Hover Text
+        colors_hex = ['#%02x%02x%02x' % tuple(rgb) for rgb in kmeans.cluster_centers_.astype(int)]
 
-    fig = go.Figure()
-    st.write(f"Following are the top-{user_number} dominant colors in the image you uploaded")
-    for i, color_hex in enumerate(colors_hex):
-        fig.add_trace(go.Bar(
-            x=[i],
-            y=[1],
-            marker=dict(color=color_hex),
-            hoverinfo="text",
-            hovertemplate=f"rgb({', '.join(map(str, kmeans.cluster_centers_[i].astype(int)))})",
-            showlegend=False
-        ))
+        fig = go.Figure()
+        st.write(f"Following are the top-{user_number} dominant colors in the image you uploaded")
+        for i, color_hex in enumerate(colors_hex):
+            fig.add_trace(go.Bar(
+                x=[i],
+                y=[1],
+                marker=dict(color=color_hex),
+                hoverinfo="text",
+                hovertemplate=f"rgb({', '.join(map(str, kmeans.cluster_centers_[i].astype(int)))})",
+                showlegend=False
+            ))
 
-    fig.update_xaxes(showticklabels=False)
-    fig.update_yaxes(showticklabels=False)
+        fig.update_xaxes(showticklabels=False)
+        fig.update_yaxes(showticklabels=False)
 
-    fig.update_layout(
-        plot_bgcolor='rgba(0,0,0,0)',
-        paper_bgcolor='rgba(0,0,0,0)',
-        height=125,
-        margin=dict(t=0, b=0, l=0, r=0),
-        bargap=0  # Set the gap between bars to 0
-    )
+        fig.update_layout(
+            plot_bgcolor='rgba(0,0,0,0)',
+            paper_bgcolor='rgba(0,0,0,0)',
+            height=125,
+            margin=dict(t=0, b=0, l=0, r=0),
+            bargap=0  # Set the gap between bars to 0
+        )
 
-    # Hide plotly options
-    st.plotly_chart(fig, use_container_width=True, config={'displayModeBar': False})
+        # Hide plotly options
+        st.plotly_chart(fig, use_container_width=True, config={'displayModeBar': False})
+
+except:
+    st.write("Kindly Upload a valid .png/.jpeg file!")
